7fea1cc793baab3535dc0451190e345cee882fa4
Rukaya Haruna <rukayaharuna18@gmail.com>
2022-06-30 16:57:16 -0700

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,7 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
@@ -12,10 +13,12 @@ int main(void)
 	long along;
 	long long alonglong;
 	float afloat;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
 	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
 	return (0);
 }
