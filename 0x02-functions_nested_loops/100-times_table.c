9362a9d45cc17d11f2972634aaa7e927e90042a9
Rukaya Haruna <rukayaharuna18@gmail.com>
2022-07-05 16:09:27 -0700

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,49 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the times table up to the integer input
- * @n: integer input
- *
- * Return: void
+ * print_times_table - prints time table
+ * @n : use in times table
+ * Return:void
  */
-
 void print_times_table(int n)
 {
-	int a, b, c, d, e, f;
+	int a = 0, rep, b;
 
-	if (n >= 0 && n < 15)
+	if (n < 0 || n > 15)
+		return;
+
+	while (a <= n)
 	{
-		for (a = 0; a <=  n; a++)
+		for (b = 0; b <= n; b++)
 		{
-			for (b = 0; b <= n; b++)
+			rep = a * b;
+			if (b == 0)
+				_putchar('0' + rep);
+			else if (rep < 10)
 			{
-				c = a * b; d = c / 100; e = (c /  10) % 10;
-				f = (c % 100) % 10;
-				if (b == 0)
-				{
-				_putchar('0');
-				}
-				else if (c < 10)
-				{
-				_putchar(' '); _putchar(' ');
-				_putchar('0' + f);
-				}
-				else if (c < 100)
-				{
 				_putchar(' ');
-				_putchar('0' + e); _putchar('0' + f);
-				}
-				else
-				{
-				_putchar('0' + d);
-				_putchar('0' + e);
-				_putchar('0' + f);
-				}
-				if (b < n)
-				{
-				_putchar(','); _putchar(' ');
-				}
-				else
-				_putchar('\n');
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else if (rep < 100)
+			{
+				_putchar (' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < n)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
+		_putchar('\n');
+		a++;
 	}
 }
